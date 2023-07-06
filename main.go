62ddd0736a19e116eb2b522b6a78e009e0181fab
emeka-chukwu <emekapascal11@gmail.com>
2023-07-06 10:32:21 +0100

    updates

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	db "blog-api/db/sqlc"
 	"blog-api/util"
 
-	"github.com/golang-migrate/migrate"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
@@ -40,16 +39,16 @@ func main() {
 
 }
 
-func runDBMigration(migrationURL string, dbSource string) {
-	migration, err := migrate.New(migrationURL, dbSource)
-	if err != nil {
-		log.Fatal().Msgf("cannot create new migrate instance: %w", err)
-	}
-	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatal().Msg("failed to run migration up:")
-	}
-	log.Info().Msg("db migrated successfully")
-}
+// func runDBMigration(migrationURL string, dbSource string) {
+// 	migration, err := migrate.New(migrationURL, dbSource)
+// 	if err != nil {
+// 		log.Fatal().Msgf("cannot create new migrate instance: %w", err)
+// 	}
+// 	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+// 		log.Fatal().Msg("failed to run migration up:")
+// 	}
+// 	log.Info().Msg("db migrated successfully")
+// }
 
 func runGinServer(config util.Config, store db.Store) {
 	server, err := app.InitializeServer(config, store)
